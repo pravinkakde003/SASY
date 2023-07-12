37de4554d84e45abd3d2a0cb5a0c6598982d5c79
PravinK <pravin.kakde003@gmail.com>
2023-07-12 11:36:17 +0530

    Moved Service to activity

--- app/src/main/java/com/sasy/nontag/ui/DetailActivity.kt
+++ app/src/main/java/com/sasy/nontag/ui/DetailActivity.kt
@@ -1,7 +1,13 @@
 package com.sasy.nontag.ui
 
+import android.bluetooth.BluetoothAdapter
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import android.os.Bundle
 import android.os.Handler
+import android.os.IBinder
 import android.os.Looper
 import android.util.Log
 import androidx.activity.viewModels
@@ -15,10 +21,25 @@ import com.sasy.nontag.ui.fragments.RangesFragment
 import com.sasy.nontag.utils.AppUtils
 import com.sasy.nontag.utils.Constants
 import com.sasy.nontag.utils.bluetooth_utils.BluetoothState
+import com.sasy.nontag.utils.bluetooth_utils.SerialListener
+import com.sasy.nontag.utils.bluetooth_utils.SerialService
+import com.sasy.nontag.utils.bluetooth_utils.SerialSocket
 import com.sasy.nontag.utils.replaceFragment
 import com.sasy.nontag.utils.setTextColorRes
+import com.sasy.nontag.utils.showToast
+import java.util.ArrayDeque
 
-class DetailActivity : AppCompatActivity() {
+class DetailActivity : AppCompatActivity(), ServiceConnection, SerialListener {
+    private enum class Connected {
+        False, Pending, True
+    }
+
+    //    private var deviceAddress: String? = null
+    private var service: SerialService? = null
+    private var connected = Connected.False
+    private var initialStart = true
+    private var isResumed = true
+
     private lateinit var binding: ActivityDetailsBinding
 
     private val dashboardViewModel: DashboardViewModel by viewModels()
@@ -35,13 +56,138 @@ class DetailActivity : AppCompatActivity() {
             intent.getStringExtra(Constants.SELECTED_DEVICE_ICON_TYPE)
                 ?.let { deviceIcon -> dashboardViewModel.setSelectedDeviceIcon(deviceIcon) }
         }
+        bindService(
+            Intent(this, SerialService::class.java),
+            this,
+            Context.BIND_AUTO_CREATE
+        )
         setMenuRecyclerView()
         setupToolbar()
     }
 
+    override fun onStart() {
+        super.onStart()
+        if (service != null) service?.attach(this) else startService(
+            Intent(
+                this@DetailActivity,
+                SerialService::class.java
+            )
+        )
+    }
+
     override fun onResume() {
         super.onResume()
+        if (initialStart && service != null) {
+            initialStart = false
+            isResumed = false
+            runOnUiThread { connect() }
+        }
         dashboardViewModel.setBluetoothState(BluetoothState.ConnectingState)
+    }
+
+    override fun onStop() {
+        try {
+            if (service != null && !isChangingConfigurations) service!!.detach()
+            unbindService(this)
+        } catch (ignored: Exception) {
+        }
+        super.onStop()
+    }
+
+    override fun onDestroy() {
+        if (connected != Connected.False) disconnect()
+        stopService(Intent(this@DetailActivity, SerialService::class.java))
+        super.onDestroy()
+    }
+
+    fun send(str: String) {
+        if (connected != Connected.True) {
+            showToast("not connected")
+            dashboardViewModel.setBluetoothState(BluetoothState.Error("Not connected."))
+            return
+        }
+        try {
+            val message = "${Constants.SET_XRANGE} $str"
+            val data: ByteArray = message.toByteArray()
+            service?.write(data)
+        } catch (e: Exception) {
+            onSerialIoError(e)
+        }
+    }
+
+
+    override fun onServiceConnected(name: ComponentName, binder: IBinder) {
+        service = (binder as SerialService.SerialBinder).service
+        service?.attach(this)
+        if (initialStart && isResumed) {
+            initialStart = false
+            isResumed = false
+            runOnUiThread { connect() }
+        }
+    }
+
+    override fun onServiceDisconnected(name: ComponentName) {
+        service = null
+    }
+
+    private fun connect() {
+        try {
+            val bluetoothAdapter = BluetoothAdapter.getDefaultAdapter()
+            val device = bluetoothAdapter.getRemoteDevice(dashboardViewModel.selectedDeviceAddress.value)
+            status("connecting...")
+            dashboardViewModel.setBluetoothState(BluetoothState.ConnectingState)
+            connected = Connected.Pending
+            val socket = SerialSocket(applicationContext, device)
+            service!!.connect(socket)
+        } catch (e: Exception) {
+            onSerialConnectError(e)
+        }
+    }
+
+    private fun disconnect() {
+        connected = Connected.False
+        service?.disconnect()
+    }
+
+    override fun onSerialConnect() {
+        status("connected")
+        dashboardViewModel.setBluetoothState(BluetoothState.ConnectedState)
+        connected = Connected.True
+    }
+
+    override fun onSerialConnectError(e: Exception) {
+        status("connection failed: " + e.message)
+        dashboardViewModel.setBluetoothState(BluetoothState.Error("Connection failed: " + e?.message))
+        disconnect()
+    }
+
+    override fun onSerialRead(data: ByteArray) {
+        val datas = ArrayDeque<ByteArray>()
+        datas.add(data)
+        receive(datas)
+    }
+
+    override fun onSerialRead(datas: ArrayDeque<ByteArray>) {
+        receive(datas)
+    }
+
+    override fun onSerialIoError(e: Exception?) {
+        status("connection lost: " + e?.message)
+        dashboardViewModel.setBluetoothState(BluetoothState.Error("Connection lost: " + e?.message))
+        disconnect()
+    }
+
+    private fun status(str: String) {
+        showToast(str)
+    }
+
+    private fun receive(datas: ArrayDeque<ByteArray>) {
+        for (data in datas) {
+            var msg = String(data)
+            if (msg.isNotEmpty()) {
+                showToast(msg)
+            }
+        }
     }
 
     private fun observeState() {
