3b05e22ea39765b434ba1dbdfcd8dd4e595eacba
PravinK <pravin.kakde003@gmail.com>
2023-07-12 12:01:45 +0530

    removed unused imports

--- app/src/main/java/com/sasy/nontag/ui/fragments/RangesFragment.kt
+++ app/src/main/java/com/sasy/nontag/ui/fragments/RangesFragment.kt
@@ -5,17 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.activityViewModels
 import com.sasy.nontag.databinding.FragmentRangesBinding
-import com.sasy.nontag.ui.DashboardViewModel
 import com.sasy.nontag.ui.DetailActivity
 import com.sasy.nontag.utils.showToast
 
 
 class RangesFragment : Fragment() {
-
     private lateinit var binding: FragmentRangesBinding
-    private val sharedViewModel: DashboardViewModel by activityViewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
